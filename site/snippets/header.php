640d63600eee6498ac4412858d9e757d2c612a9b
Bruno Mokross <bruno@Brunos-MacBook-Pro.local>
2020-07-26 20:59:20 +0200

    header improvement

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $site->title(); ?></title>
 
-    <?= css('assets/css/style.css'); ?>
-    <?= js('assets/js/script.css'); ?>
+    <?= css([
+        'assets/css/style.css',
+        ]); ?>
+        
+    <?= js([
+        'assets/js/script.css',
+        ]); ?>
 </head>
 <body>
