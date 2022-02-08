b7e54134f159ca4a6e9e7bf79d9508031fca758d
Bruno <38008341+moevbiz@users.noreply.github.com>
2022-02-08 16:38:50 +0100

    rploy ootb; remove panel view extended plugin; add url config for ngrok share

--- public/site/config/config.example.test.php
+++ public/site/config/config.example.test.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    // valet share config for ngrok
+    'url' => isset($_SERVER['HTTP_X_FORWARDED_HOST']) ?  'https://' . $_SERVER['HTTP_X_FORWARDED_HOST'] : 'https://example.test',
     'ready' => function ($kirby) {
         return ['debug' => true];
     },
