072b4163f403594a1313cf38f97cfe46e256d7df
Bruno <38008341+moevbiz@users.noreply.github.com>
2022-02-20 12:24:00 +0100

    ngrok

--- public/site/config/config.example.test.php
+++ public/site/config/config.example.test.php
@@ -2,7 +2,7 @@
 
 return [
     // valet share config for ngrok
-    'url' => isset($_SERVER['HTTP_X_FORWARDED_HOST']) ?  'https://' . $_SERVER['HTTP_X_FORWARDED_HOST'] : 'https://example.test',
+    // 'url' => isset($_SERVER['HTTP_X_FORWARDED_HOST']) ?  'https://' . $_SERVER['HTTP_X_FORWARDED_HOST'] : 'https://example.test',
     'ready' => function ($kirby) {
         return ['debug' => true];
     },
