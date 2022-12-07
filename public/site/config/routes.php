b48eb8cdd9b81f468a017a8c7d394d155caac6cb
Bruno <moev.biz@moev.biz>
2022-12-07 16:05:32 +0100

    $this->next();

--- public/site/config/routes.php
+++ public/site/config/routes.php
@@ -5,6 +5,7 @@ return [
         'pattern' => '(:all)',
         'action' => function($page) {
             if (option('prelaunch') && !kirby()->user()) go('/panel');
+            $this->next();
         }
     ]
 ];
