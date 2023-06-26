7a4d5a6fbbfd6c8fb2c2318839064bd49821d588
Bruno <moev.biz@moev.biz>
2023-06-26 11:48:49 +0200

    add phpinfo route for logged in admins

--- public/site/config/routes.php
+++ public/site/config/routes.php
@@ -7,5 +7,15 @@ return [
             if (option('prelaunch') && !kirby()->user()) go('/panel');
             $this->next();
         }
-    ]
+    ],
+    [
+        'pattern' => 'phpinfo',
+        'language' => '*',
+        'action' => function () {
+            if (kirby()->user() && kirby()->user()->isAdmin()) {
+                phpinfo();
+            }
+            return $this->next();
+        }
+    ],
 ];
