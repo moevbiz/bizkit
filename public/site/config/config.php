1153ad7afc3930c574a1106a5e8b205efae4f850
Bruno <moev.biz@moev.biz>
2023-09-23 15:27:32 +0200

    remove dotenv code from config

--- public/site/config/config.php
+++ public/site/config/config.php
@@ -1,9 +1,5 @@
 <?php
 
-$dotenv = new \Dotenv\Dotenv(__DIR__ . '/../..');
-$dotenv->load();
-// get environment variable with $_ENV['KEY']
-
 return [
     'hooks' => include 'hooks.php',
     'routes' => include 'routes.php',
